fcfcbd860a46c18a9382e4bcc0e778c91c1949b1
sissie <sissiescofield@outlook.com>
2015-08-17 18:56:24 +0800

    DELETE and ADD have finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var sortByOrder = require('./module/sort.js');
 var integrate = require('./module/integrate.js');
 
+
 var express = require('express');
 var	app = express();
 
@@ -9,12 +10,16 @@ app.set('view engine', 'html');	// 用hbs作为模版引擎
 app.set('views', __dirname + '/views');	// 模版所在路径
 app.engine('html', hbs.__express);
 
-app.use(express.static('bower_components'));
+app.use(express.static('node_modules'));
 app.use(express.static('public'));
 
+var body_parser = require('body-parser');
+app.use(body_parser.urlencoded({
+  extended: true
+}));
 
 var mysql = require('mysql');
-app.get('*', function(res, req, next) {
+app.all('*', function(res, req, next) {
   connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -49,17 +54,42 @@ app.get('/sort', function(req, res) {
   var key = req.query.sortKey;
   var ord = req.query.order;
   res.send(sortByOrder(key, ord, integrated));
-  connection.end();
 });
 
-var deleteSQL = 'delete from score where student_id = ';
 app.get('/delete',function(req, res) {
   var id = req.query.id;
-  connection.query(deleteSQL + id, function (err,status, res) {
+  var deleteSQL = 'delete from score where student_id = ' + id;
+  connection.query(deleteSQL, function (err,rows) {
     if (err) {
-      res.send(err);
-    } 
+      throw err;
+    }
+    res.send("yeah");
     connection.end();
+  });
+});
+
+  // var addSQL2 = '';
+app.post('/add',function(req, res) {
+  var name = req.body.name;
+  var chinese = req.body.chinese;
+  var math = req.body.math;
+  var english = req.body.english;
+
+  var addSQL1 = 'insert into student(name) values("' + name + '")';
+  connection.query(addSQL1, function (err,rows) {
+    if (err) {
+      throw err;
+    }
+    var id = rows.insertId;
+    var addSQL2 = 'insert into score(student_id,course_id,score) values(' + id + ',' + '1,' +  chinese + '),' + '(' + id + ',' + '2,' +  math + '),' + '(' + id + ',' + '3,' +  english + ');';
+    connection.query(addSQL2, function (err,rows) {
+      if (err) {
+        throw err;
+      }
+      res.send({id:id});
+      connection.end();
+    });
+
   });
 });
 
--- public/main.js
+++ public/main.js
@@ -4,8 +4,9 @@ $(function() {
 
   function makeTable(collection) {
     var out = collection.map(function(val) {
-      return ("<tr>" + "<td>" + val.id + "</td>" + "<td>" + val.name + "</td>" + "<td>" +
-      val.chinese + "</td>" + "<td>" + val.math + "</td>" + "<td>" + val.english + "</td>" + "<td>" + "删除" + "</td>" + "</tr>");
+      var id  = val.id;
+      return ("<tr id=" + id + ">" + "<td>" + val.name + "</td>" + "<td>" +
+      val.chinese + "</td>" + "<td>" + val.math + "</td>" + "<td>" + val.english + "</td>" + "<td class='delete' data-id=" + id + ">" + "删除" + "</td>" + "</tr>");
     }).join();
 
     return out;
@@ -15,8 +16,7 @@ $(function() {
     var key = $(this).data('tip');
     $.get('/sort', {sortKey: key, order: order}, function(result) {
       sorted = makeTable(result);
-      $("tbody").empty();
-      $("tbody").append(sorted);
+      $("tbody").html(sorted);
     });
 
     if(order === "asc") {
@@ -26,11 +26,41 @@ $(function() {
     }
   });
 
-  $(".table-score-tbody").on("click", "td", function() {
+  $("tbody").on("click", ".delete", function() {
     var key = $(this).data('id');
     $.get('/delete', {id: key}, function(res) {
-        $.get('/', function(res) {});
+      var a = confirm("Are you sure to delete？");
+        if (a) {
+          $("tbody #" + key).remove();
+        }
     });
   });
+
+  $("tfoot").on("click", ".add", function() {
+    var add_name = $("#add_name").val();
+    var add_chinese = $("#add_chinese").val();
+    var add_math = $("#add_math").val();
+    var add_english = $("#add_english").val();
+
+    $.ajax({
+      url: '/add',
+      type: "POST",
+      data:{name:add_name, chinese:add_chinese, math:add_math, english:add_english},
+      success:function(resq) {
+        var id = resq.id;
+        var newRecord = "<tr id=" + id + ">" + id + "<td>" + add_name + "</td>" + "<td>" +
+        add_chinese + "</td>" + "<td>" + add_math + "</td>" + "<td>" + add_english + "</td>" +
+        "<td class='delete' data-id=" + id + ">" + "删除" + "</td>" + "</tr>";
+        $("tbody").append(newRecord);
+
+        $("#add_name").val('');
+        $("#add_chinese").val('');
+        $("#add_math").val('');
+        $("#add_english").val('');
+      }
+    });
+});
+
+
 
 });
